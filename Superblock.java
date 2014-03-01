78900724761cc12d80165bd15e03b0d3c1e505be
nguyentong77 <nguyentong77@yahoo.com>
2014-03-01 14:38:40 -0800

    Create Superblock.java

---
+++ Superblock.java
@@ -0,0 +1,10 @@
+/* Joseph Schooley & Nguyen Tong, CSS 430, Professor Sung
+   CSS430 Final Project - File System
+   Superblock Class
+*/
+
+public class Superblock {
+   public int totalBlocks;
+   public int totalInodes;
+   public int freeList;
+}
