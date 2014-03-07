d6a7e09d6a59ee4d26315421f74ce70c2d20dd18
nguyentong77 <nguyentong77@yahoo.com>
2014-03-07 12:29:05 -0800

    Update FileSystem.java
    Added the FileSystem class for calling functions so that user threads can't directly call Kernel or SysLib function calls. 
    
    FileSystem instantiate SuperBlock, the Directory, as well as the FileTable.

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -6,5 +6,61 @@ import java.util.*;
 */
 
 public class FileSystem {
-
+   private SuperBlock superBlock;
+   private Directory directory;
+   private FileTable fileTable;
+   
+   public FileSystem()  {
+      
+   }
+   
+   public FileSystem(int blocks) {
+      superBlock = new SuperBlock(blocks);
+      
+      directory = new Directory(); 
+      
+      fileTable = new FileTable(directory);
+   
+   }
+   
+   public void sync()   {
+      
+   }
+   
+   public boolean format() {
+      
+   }
+   
+   public FileTableEntry open() {
+      
+   }
+   
+   public boolean close()  {
+      
+   }
+   
+   public int fsize() {
+      
+   }
+   
+   public int read ()   {
+      
+   }
+   
+   public int write()   {
+      
+   }
+   
+   
+   public boolean delete() {
+      
+   }
+   
+   public int seek() {
+      
+   }
+   
+   public boolean deallocateBlocks()   {
+      
+   }
 }
